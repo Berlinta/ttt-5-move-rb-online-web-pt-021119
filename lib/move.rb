bc0936eae7ada6919cfc16bc3811a564fe0f0603
Nash Brancic <nashfft@gmail.com>
2019-03-06 03:15:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,13 +4,9 @@ def display_board(board)
   puts "What is your move?"
   user_input = gets.strip
   index = input_to_index(user_input)
-  move(board, index)
-  display_board(board)
   puts "What is your next move?"
   user_input = gets.strip
   index = input_to_index(user_input)
-  move(board, index)
-  display_board(board)
 end
 
 def input_to_index(user_input)
