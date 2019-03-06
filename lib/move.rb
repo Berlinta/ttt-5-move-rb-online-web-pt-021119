119709614527691758097061c0b3a02ad64fb831
Nash Brancic <nashfft@gmail.com>
2019-03-06 03:38:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,9 @@
-
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "	  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"	  puts "-----------"
+@@ -6,4 +7,12 @@ def display_board(board)
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "	  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end	end
 
 
 
