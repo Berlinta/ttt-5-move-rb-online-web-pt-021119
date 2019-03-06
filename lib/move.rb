26b1d0ed52322ed5a388a7f93e32d00e004f1af5
Nash Brancic <nashfft@gmail.com>
2019-03-06 02:47:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,16 +1,33 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
-def input_to_index(user_input)
-  user_input = user_input.to_i
-  user_input = user_input - 1
-end
 
-def move(board, index, character = "X")
-  board[index] = character
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def input_to_index(user_input)
+#   user_input = user_input.to_i
+#   user_input = user_input - 1
+# end
+
+# def move(board, index, character = "X")
+#   board[index] = character
+# end
