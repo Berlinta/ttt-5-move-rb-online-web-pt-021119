5d6ae3ad495a4647147dc9694c4a9bf364764dbf
Nash Brancic <nashfft@gmail.com>
2019-03-06 03:20:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,5 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
 end
 
 def input_to_index(user_input)
@@ -15,3 +11,16 @@ def move(board, index, character = "X")
   board[index] = character
 end
 
+  puts "Welcome to Tic Tac Toe!"
+  
+  board = [" "," "," "," "," "," "," "," "," "]
+
+  puts "Where would you like to go?"
+
+  input= gets.strip
+
+  index=input_to_index(input)
+
+  move(board, index, player_character = "X")
+
+  display_board(board) 
