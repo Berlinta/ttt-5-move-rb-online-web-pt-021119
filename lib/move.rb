2f8806cd5609b5d4bb351cfaba9a25ad883d4483
Nash Brancic <nashfft@gmail.com>
2019-03-06 03:01:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,7 +1,7 @@
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   puts "Welcome to Tic Tac Toe!"
-  gets = board
+  user_input = gets.chomp
 end
 
 def input_to_index(user_input)
