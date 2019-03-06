0f436757a9a16a91a2d497cc1dac9871a5a446a3
Nash Brancic <nashfft@gmail.com>
2019-03-06 03:39:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ def input_to_index(input)
    index = input.to_i - 1
 end
 
- def move(board,converted_input,value="X")
+ def move(board,converted_input,value = "X")
  board[converted_input] = value
 end
 
