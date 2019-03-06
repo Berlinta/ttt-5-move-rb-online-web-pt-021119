e51abf2977321ab5280d88282176e5e1acb46676
Nash Brancic <nashfft@gmail.com>
2019-03-06 02:49:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -23,12 +23,11 @@
 
 
 
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  user_input = user_input - 1
+end
 
-# def input_to_index(user_input)
-#   user_input = user_input.to_i
-#   user_input = user_input - 1
-# end
-
-# def move(board, index, character = "X")
-#   board[index] = character
-# end
+def move(board, index, character = "X")
+  board[index] = character
+end
